ccce82c8ff7ad5dd66647acb7d27bea4737dd9fa
Catherine <60444425+MonkeyKingBar-bit@users.noreply.github.com>
2021-08-27 15:14:53 +0300

    fix: change files after review

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-debugger */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/extensions */
 /* eslint-disable react/jsx-filename-extension */
@@ -7,33 +8,25 @@ import './App.css';
 import Header from './components/Header/Header';
 import Cards from './components/Card/Cards';
 
+interface CardData {
+  id: number;
+  title: string;
+  text: string;
+}
+const cardData: Array<CardData> = [
+  {
+    id: 1,
+    title: 'Heading',
+    text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat, harum eligendi accusantium tempora provident iste nam eius id sit iusto accusamus cum nostrum, consequatur eaque dicta doloribus hic ex voluptate!',
+  },
+];
+
 const App = () => (
   <div className="app-wrapper">
     <Header />
     <div className="app-content">
-      <Cards
-        title="Heading1"
-        text="Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nihil soluta, quae corrupti quo voluptatem eius sint sed
-                    accusantium eveniet animi vero maiores dignissimos obcaecati
-                    blanditiis dolorem quos esse sequi nostrum?"
-      />
-      <Cards
-        title="Heading2"
-        text="Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nihil soluta, quae corrupti quo voluptatem eius sint sed
-                    accusantium eveniet animi vero maiores dignissimos obcaecati
-                    blanditiis dolorem quos esse sequi nostrum?"
-      />
-      <Cards
-        title="Heading3"
-        text="Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Nihil soluta, quae corrupti quo voluptatem eius sint sed
-                    accusantium eveniet animi vero maiores dignissimos obcaecati
-                    blanditiis dolorem quos esse sequi nostrum?"
-      />
+      <Cards cardData={cardData} />
     </div>
   </div>
 );
-
 export default App;
--- src/components/Card/Cards.tsx
+++ src/components/Card/Cards.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable no-use-before-define */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/jsx-filename-extension */
 // import Button from "@material-ui/core/Button";
+import React from 'react';
 import Card from '@material-ui/core/Card';
 // import CardActions from "@material-ui/core/CardActions";
 import CardContent from '@material-ui/core/CardContent';
@@ -11,7 +13,6 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import { useState } from 'react';
 
 const useStyles = makeStyles((theme: Theme) => ({
   cardGrid: {
@@ -30,38 +31,48 @@ const useStyles = makeStyles((theme: Theme) => ({
     flexGrow: 1,
   },
 }));
-const Cards = (props: any) => {
+interface CardProps {
+  cardData: any;
+}
+const cards = [1, 2, 3];
+
+const Cards: React.FC<CardProps> = (props: CardProps) => {
   const classes = useStyles();
-  const [text, setText] = useState(props.text);
+  const { cardData } = props;
   return (
     <main>
       <Container className={classes.cardGrid} maxWidth="md">
         <Grid container spacing={4}>
-          <Grid item xs={12} sm={12} md={12}>
-            <Card className={classes.card}>
-              <CardMedia
-                className={classes.cardMedia}
-                image="https://source.unsplash.com/random"
-                title="Image title"
-              />
-              <CardContent className={classes.cardContent}>
-                <Typography gutterBottom variant="h5" component="h2">
-                  {props.title}
-                </Typography>
-                <Typography>
-                  {props.text}
-                </Typography>
-              </CardContent>
-              {/* <CardActions>
-                    <Button size="small" color="primary">
-                      View
-                    </Button>
-                    <Button size="small" color="primary">
-                      Edit
-                    </Button>
-                  </CardActions> */}
-            </Card>
-          </Grid>
+          {cards.map((card) => (
+            <Grid item key={card} xs={12} sm={6} md={4}>
+              <Card className={classes.card}>
+                <CardMedia
+                  className={classes.cardMedia}
+                  image="https://source.unsplash.com/random"
+                  title="Image title"
+                />
+                {cardData.map((data: any) => (
+                  <CardContent key={data} className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {data.title}
+                    </Typography>
+                    <Typography>
+                      {data.text}
+                    </Typography>
+                  </CardContent>
+                ))}
+
+                {/* <CardActions>
+                  <Button size="small" color="primary">
+                    View
+                  </Button>
+                  <Button size="small" color="primary">
+                    Edit
+                  </Button>
+                </CardActions> */}
+              </Card>
+            </Grid>
+          ))}
         </Grid>
       </Container>
     </main>

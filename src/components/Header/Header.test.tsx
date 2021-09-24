4291cd0cd6bfc08a142331274dfb36ee4347ba09
Catherine <60444425+MonkeyKingBar-bit@users.noreply.github.com>
2021-09-24 16:05:53 +0300

    fix: change testing file

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,12 +1,15 @@
 import { render, screen } from "@testing-library/react";
 // import userEvent from "@testing-library/user-event";
-import Header from "../src/components/Header/Header";
+import Header from "./Header";
 
 describe("Header component", () => {
   test("should open modal window if the add button was clicked", () => {
+    // Arrange
     render(<Header />);
+    // Act
     const addButton = screen.getByTestId("addButton");
     // userEvent.click(addButton);
+    // Assert
     expect(addButton).toBeInTheDocument();
   });
 });
